f90bead7736998fa60f0233b1bdbf253ecf0b528
vsvankhede <vijay.s.vankhede@gmail.com>
2016-03-08 09:01:06 +0530

    Add content provider

--- app/src/main/java/com/vstechlab/popularmovies/data/db/MoviesContract.java
+++ app/src/main/java/com/vstechlab/popularmovies/data/db/MoviesContract.java
@@ -1,16 +1,31 @@
 package com.vstechlab.popularmovies.data.db;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
  * Define table and column name of Movies database.
  */
 public class MoviesContract {
+    public static final String CONTENT_AUTHORITY = "com.vstechlab.popularmovies.app";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_FAVORITE = "favorite";
 
     /**
      * Inner class that defines the table content of Favorite movies
      */
     public static final class FavoriteMovies implements BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_FAVORITE).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_FAVORITE;
+
         public static final String TABLE_NAME = "favorite";
 
         public static final String COLUMN_MOVIE_KEY = "movie_id";
@@ -19,5 +34,6 @@ public class MoviesContract {
         public static final String COLUMN_POSTER = "poster";
         public static final String COLUMN_SUMMARY = "summary";
         public static final String COLUMN_VOTE_AVG = "vote_avg";
+
     }
 }
---
+++ app/src/main/java/com/vstechlab/popularmovies/data/db/MoviesProvider.java
@@ -0,0 +1,57 @@
+package com.vstechlab.popularmovies.data.db;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+
+public class MoviesProvider extends ContentProvider {
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private MoviesDbHelper mOpenHelper;
+
+    static final int FAVORITE = 100;
+
+    static UriMatcher buildUriMatcher() {
+
+        return null;
+    }
+
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = new MoviesDbHelper(getContext());
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
+        return null;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        final int match = sUriMatcher.match(uri);
+
+        switch (match) {
+            case FAVORITE:
+                return MoviesContract.FavoriteMovies.CONTENT_TYPE;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String s, String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+        return 0;
+    }
+}
